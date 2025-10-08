da00cc33775b346f477747354a3f3d93717cb1e4
2018wzh <wzh20070720@outlook.com>
2025-10-08 18:49:08 +0800

    [kernel][init] Add logging for vm

--- kernel/src/init/vm.rs
+++ kernel/src/init/vm.rs
@@ -7,8 +7,9 @@ static VM_INIT_ONCE: Once<()> = Once::new();
 pub fn vm_init(hartid: usize) {
     VM_INIT_ONCE.call_once(|| {
         init_kernel_vm();
-        crate::printk!("VM: Root page table built by hart{}\n", hartid);
+        printk!("VM: Root page table built by hart {}", hartid);
     });
 
     switch_to_kernel_vm();
+    printk!("VM: Hart {} switched to kernel page table", hartid);
 }
